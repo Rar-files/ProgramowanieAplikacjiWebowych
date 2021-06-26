614fde1d2c5d2bcd1ae2462fc0cf485ab340fd7a
rar-files <dwidlak@outlook.com>
2021-06-26 16:16:22 +0200

    clearApp

--- Lab4_Lab5-NoteKeep/webpack.common.js
+++ Lab4_Lab5-NoteKeep/webpack.common.js
@@ -3,7 +3,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
